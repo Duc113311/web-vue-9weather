2f4f3223476add3e19a65496a385dfbb3594515b
Duc Nguyen <ducnv@bachasoft.com>
2024-10-19 10:22:18 +0700

    cap nhat

--- 9weather_v2/build-timestamp.js
+++
@@ -1,4 +0,0 @@
-const { execSync } = require("child_process");
-const timestamp = Date.now(); // Or use another method to generate a timestamp
-process.env.BUILD_TIMESTAMP = timestamp;
-execSync("vue-cli-service build", { stdio: "inherit" });
