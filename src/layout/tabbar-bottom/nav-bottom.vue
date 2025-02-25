189526211289fd60940f16c18b21bf491925fae8
asctechsoft <asctechsoft888@gmail.com>
2025-02-25 17:56:34 +0700

    cap nhat ui

--- src/layout/tabbar-bottom/nav-bottom.vue
+++ src/layout/tabbar-bottom/nav-bottom.vue
@@ -5,43 +5,6 @@
         <div class="w-full h-full flex justify-between items-center">
           <!-- left -->
           <div class="md:flex hidden items-center gap-4">
-            <div class="h-district pt-5 pb-5 hidden lg:block">
-              <div class="flex items-center gap-2 cursor-pointer">
-                <svg
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M6 22V4C6 3.46957 6.21071 2.96086 6.58579 2.58579C6.96086 2.21071 7.46957 2 8 2H16C16.5304 2 17.0391 2.21071 17.4142 2.58579C17.7893 2.96086 18 3.46957 18 4V22M6 22H18M6 22H4C3.46957 22 2.96086 21.7893 2.58579 21.4142C2.21071 21.0391 2 20.5304 2 20V14C2 13.4696 2.21071 12.9609 2.58579 12.5858C2.96086 12.2107 3.46957 12 4 12H6M18 22H20C20.5304 22 21.0391 21.7893 21.4142 21.4142C21.7893 21.0391 22 20.5304 22 20V11C22 10.4696 21.7893 9.96086 21.4142 9.58579C21.0391 9.21071 20.5304 9 20 9H18M10 6H14M10 10H14M10 14H14M10 18H14"
-                    stroke="var(--color-icon-df)"
-                    stroke-width="2"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  />
-                </svg>
-
-                <span class="txt_regular_17">{{ $t("District_City") }}</span>
-                <svg
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M18 9L12 15L6 9"
-                    stroke="var(--color-icon-df)"
-                    stroke-width="2"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  />
-                </svg>
-              </div>
-            </div>
-
             <div
               class="flex items-center gap-2 cursor-pointer"
               @click="onClickShowWidget()"
